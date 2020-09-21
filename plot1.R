d95ab8b1f5780ea9e00cfcfe7e73a2d144e6fc56
Suchitra <psuchitra11@gmail.com>
2020-09-21 18:24:22 +0530

    all 8 done

---
+++ plot1.R
@@ -0,0 +1,15 @@
+#reading Data into R
+data<-read.table("D:\\EDUCATION\\DATA SCIENCE JH\\ExData_Plotting1\\household_power_consumption.txt",sep=";",skip=1)
+cnames<-read.table("D:\\EDUCATION\\DATA SCIENCE JH\\ExData_Plotting1\\household_power_consumption.txt",sep=";",nrows=1)
+colnames(data)<-cnames
+data<-transform(data,Date=as.Date(Date,"%d/%m/%Y"),Time=as.hms(Time),Global_active_power=as.numeric(Global_active_power),
+          Global_reactive_power=as.numeric(Global_reactive_power),Global_intensity=as.numeric(Global_intensity),
+          Sub_metering_1=as.numeric(Sub_metering_1),Sub_metering_2=as.numeric(Sub_metering_2))
+#subsetting the data from dates - 2007-02-01 and 2007-02-02
+febdata<-filter(data, Date >=("2007-02-01") & Date <= ("2007-02-02"))
+
+#PLOT 1 code:
+dev.new(width=480,height=480,unit="px")
+hist(febdata$Global_active_power,col="red",main="Global Active Power",xlab = "Global Active Power(kilowatts")
+dev.copy(png,"plot1.png")
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,18 @@
+#reading Data into R
+data<-read.table("D:\\EDUCATION\\DATA SCIENCE JH\\ExData_Plotting1\\household_power_consumption.txt",sep=";",skip=1)
+cnames<-read.table("D:\\EDUCATION\\DATA SCIENCE JH\\ExData_Plotting1\\household_power_consumption.txt",sep=";",nrows=1)
+colnames(data)<-cnames
+data<-transform(data,Date=as.Date(Date,"%d/%m/%Y"),Time=as.hms(Time),Global_active_power=as.numeric(Global_active_power),
+                Global_reactive_power=as.numeric(Global_reactive_power),Global_intensity=as.numeric(Global_intensity),
+                Sub_metering_1=as.numeric(Sub_metering_1),Sub_metering_2=as.numeric(Sub_metering_2))
+#subsetting the data from dates - 2007-02-01 and 2007-02-02
+febdata<-filter(data, Date >=("2007-02-01") & Date <= ("2007-02-02"))
+
+#PLOT 3 CODE:
+dev.new(width=480,height=480,unit="px")
+plot(febdata$datetime,febdata$Sub_metering_1,type="l",xlab="",ylab="Energy Sub metering")
+lines(febdata$datetime,febdata$Sub_metering_2,col="red",type="l")
+lines(febdata$datetime,febdata$Sub_metering_3,type="l",col="blue")
+legend("topright",legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"),lty=1)
+dev.copy(png,"plot3.png")
+dev.off()
